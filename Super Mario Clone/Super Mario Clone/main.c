298a8853fb7192eea72fe4d8b62c5cf03dc75add
Pikachuxxxx <phani.s2909@icloud.com>
2020-06-04 16:49:22 +0530

    Added base raylib library

--- Super Mario Clone/Super Mario Clone/main.c
+++ Super Mario Clone/Super Mario Clone/main.c
@@ -7,9 +7,11 @@
 //
 
 #include <stdio.h>
+#include <raylib.h>
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+   
+
+    
 }
