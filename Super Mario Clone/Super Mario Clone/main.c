bbdcb8e045a0245d7bfb8eeb8c20189efd4ad756
Pikachuxxxx <phani.s2909@icloud.com>
2020-06-04 20:15:21 +0530

    Added Ground Drawing Function

--- Super Mario Clone/Super Mario Clone/main.c
+++ Super Mario Clone/Super Mario Clone/main.c
@@ -9,14 +9,18 @@
 #include <stdio.h>
 #include <raylib.h>
 
+const int screenWidth = 800;
+const int screenHeight = 600;
 
+void DrawGround(Texture2D groundTex, int groundHeight);
 
 int main(int argc, const char * argv[]) {
    
-    const int screenWidth = 800;
-    const int screenHeight = 600;
+
     const int playerWidth = 50;
     const int playerHeight = 100;
+    const int groundHeight = 100;
+
     
     InitWindow(screenWidth, screenHeight, "Mario clone");
     SetTargetFPS(900);
@@ -26,7 +30,11 @@ int main(int argc, const char * argv[]) {
     Color testGround = (Color){231,91,52,255};
     Color playerColor = (Color){254,240,79,255};
     
-    Texture2D groundBrick = LoadTexture("./Resources/Bricks.gif");
+    Texture2D groundBrick = LoadTexture("/Users/phanisrikar/Desktop/Brick.png");
+//    Texture2D groundBrick = LoadTexture("./Assets/Brick.png");
+
+
+    
 
     while (!WindowShouldClose())
     {
@@ -35,15 +43,12 @@ int main(int argc, const char * argv[]) {
         // Physics
         
         //Drawing
-
         BeginDrawing();
         ClearBackground(marioSkyBlue);
         DrawFPS(0, 0);
        
-        //Ground for player
-        DrawRectangle(0, 400, screenWidth, 100,testGround);
-        DrawTexture(groundBrick, 0, 0, RAYWHITE);
-
+        DrawGround(groundBrick,groundHeight);
+        
         //Collisions
         
         EndDrawing();
@@ -51,4 +56,17 @@ int main(int argc, const char * argv[]) {
 
     CloseWindow();
     return 0;
+}
+
+void DrawGround(Texture2D groundTex, int groundHeight){
+    int groundXIterations = (int)screenWidth/(float)groundTex.width;
+    int groundYIterations = (int)screenHeight/(float)groundTex.height;
+    //Ground for player
+    for(int i = 0; i < groundYIterations ; i++)
+    {
+        for(int j = 0; j < groundXIterations ; j ++)
+        {
+            DrawTexture(groundTex, groundTex.width * j,((screenHeight - groundHeight) + (groundTex.height * i)), RAYWHITE);
+        }
+    }
 }
