48b1f7d354fa35ce33c1076ccf77aa2f484948e1
Pikachuxxxx <phani.s2909@icloud.com>
2020-06-04 20:38:56 +0530

    Testing controls and added 2D camera

--- Super Mario Clone/Super Mario Clone/main.c
+++ Super Mario Clone/Super Mario Clone/main.c
@@ -19,39 +19,49 @@ int main(int argc, const char * argv[]) {
 
     const int playerWidth = 50;
     const int playerHeight = 100;
+    const int ballSpeed = 4;
     const int groundHeight = 100;
-
     
     InitWindow(screenWidth, screenHeight, "Mario clone");
-    SetTargetFPS(900);
-    
+      SetTargetFPS(900);
     
     Color marioSkyBlue = (Color){107,139,247};
     Color testGround = (Color){231,91,52,255};
     Color playerColor = (Color){254,240,79,255};
     
-    Texture2D groundBrick = LoadTexture("/Users/phanisrikar/Desktop/Brick.png");
-//    Texture2D groundBrick = LoadTexture("./Assets/Brick.png");
-
-
     
+    Vector2 playerPos = {0,0};
+    Vector2 camOffset = {0,0};
+    Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+    //Define the 2D camera here
+    Camera2D mainCam2D = (Camera2D){camOffset,ballPosition,0,1};
+
+    Texture2D groundBrick = LoadTexture("/Users/phanisrikar/Desktop/Brick.png");
 
     while (!WindowShouldClose())
     {
         // Events and Input handling
-        
+        if(IsKeyDown(KEY_RIGHT))
+            ballPosition.x += ballSpeed * 100.0f * GetFrameTime();
+        if(IsKeyDown(KEY_LEFT))
+        ballPosition.x -= ballSpeed * 100.0f * GetFrameTime();
         // Physics
         
         //Drawing
+        BeginMode2D(mainCam2D);
+
         BeginDrawing();
         ClearBackground(marioSkyBlue);
         DrawFPS(0, 0);
-       
-        DrawGround(groundBrick,groundHeight);
         
+        DrawGround(groundBrick,groundHeight);
+        DrawCircleV(ballPosition, 50, MAROON);
+
         //Collisions
         
+        
         EndDrawing();
+        EndMode2D();
     }
 
     CloseWindow();
