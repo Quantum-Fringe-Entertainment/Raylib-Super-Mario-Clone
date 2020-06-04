98de95962acf92ebd38f84313c2ba617b74e7b00
Pikachuxxxx <phani.s2909@icloud.com>
2020-06-04 16:46:56 +0530

    Project initialised

---
+++ Super Mario Clone/Super Mario Clone/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Super Mario Clone
+//
+//  Created by phani srikar on 04/06/20.
+//  Copyright © 2020 phani srikar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
